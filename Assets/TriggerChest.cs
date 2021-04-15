2b98e0f5a677316c379602a5d11f201a67011daa
manyNeurons <ferranramirezmarti@gmail.com>
2021-04-15 11:29:45 +0200

    trigger working! checkpoint xD

---
+++ Assets/TriggerChest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChest : MonoBehaviour
+{
+   
+    public Animator animator;
+    public Animator nave;
+    //public bool is_Open = false;
+    bool firstTrigger = true;
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+       // m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    //Moves this GameObject 2 units a second in the forward direction
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.T)) 
+        //     if(animator.GetBool("is_Open")) {
+        //         animator.SetBool("is_Open", false); 
+        //     } else {
+        //         animator.SetBool("is_Open", true); 
+        //         nave.SetBool("is_Triggered", true);
+        //     }
+
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        print("something");
+        
+        if(firstTrigger) {
+            firstTrigger = false;
+            animator.SetBool("first_Open", true);
+        }
+        else {
+            if(!animator.GetBool("is_Open")) {
+                animator.SetBool("is_Open", true); 
+                nave.SetBool("is_Triggered", true);
+            } else {
+                animator.SetBool("is_Open", false); 
+            }
+        }
+    }
+}
---
+++ Assets/TriggerShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerShip : MonoBehaviour
+{
+    public bool animationFinished = false;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animationFinished) animator.SetBool("is_Triggered", false); 
+    }
+}
