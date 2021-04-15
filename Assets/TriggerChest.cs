49fbc9f9c4d8df565766d5ef6a21bc479796bb5a
manyNeurons <ferranramirezmarti@gmail.com>
2021-04-15 20:04:17 +0200

    ship not waiting. cheking last commit

--- Assets/TriggerChest.cs
+++ Assets/TriggerChest.cs
@@ -2,47 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerChest : MonoBehaviour
-{
-   
-    public Animator animator;
+public class TriggerChest : MonoBehaviour {
+    public Animator chest;
     public Animator nave;
     //public bool is_Open = false;
     bool firstTrigger = true;
-    void Start()
-    {
-        //Get the Animator attached to the GameObject you are intending to animate.
-       // m_Animator = gameObject.GetComponent<Animator>();
-    }
+    void Start()  { }
 
-    //Moves this GameObject 2 units a second in the forward direction
-    void Update()
-    {
-        // if(Input.GetKeyDown(KeyCode.T)) 
-        //     if(animator.GetBool("is_Open")) {
-        //         animator.SetBool("is_Open", false); 
-        //     } else {
-        //         animator.SetBool("is_Open", true); 
-        //         nave.SetBool("is_Triggered", true);
-        //     }
+    void Update() { }
 
-    }
-
-    //Upon collision with another GameObject, this GameObject will reverse direction
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other) {
         print("something");
         
         if(firstTrigger) {
             firstTrigger = false;
-            animator.SetBool("first_Open", true);
+            chest.SetBool("first_Open", true);
         }
         else {
-            if(!animator.GetBool("is_Open")) {
-                animator.SetBool("is_Open", true); 
+            if(!chest.GetBool("is_Open")) {
+                chest.SetBool("is_Open", true); 
                 nave.SetBool("is_Triggered", true);
             } else {
-                animator.SetBool("is_Open", false); 
+                chest.SetBool("is_Open", false); 
             }
         }
     }
